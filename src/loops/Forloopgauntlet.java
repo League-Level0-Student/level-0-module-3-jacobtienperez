e99b287d1bf0cd74a971513f745ecacb0a62a174
perez.jacob <perez.jacob@SEMS-259-18.SEMS259.LAB>
2018-05-09 14:54:50 -0700

    i did forloop gauntlet

---
+++ src/loops/Forloopgauntlet.java
@@ -0,0 +1,20 @@
+package loops;
+
+public class Forloopgauntlet {
+public static void main(String[] args) {
+	
+	for(int i=0; i < 100; i+=10) {
+		  for(int j=i;j<i+10;j++) {
+			 System.out.print((i+j)+"\t");
+			 }
+			
+		  System.out.println();
+		 }
+		 
+		
+		
+	
+		
+}
+}
+
